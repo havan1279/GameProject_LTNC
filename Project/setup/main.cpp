d653b817c892628238d95055d6d2b8b2779e7fd5
ducvu25 <80148417+havan1279@users.noreply.github.com>
2024-07-28 00:54:44 +0700

    Setup moi truong

---
+++ Project/setup/main.cpp
@@ -0,0 +1,237 @@
+﻿#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <iostream>
+#include<vector>
+#include<string>
+#include<stdlib.h>
+#include<fstream>
+#include <random>
+#include <ctime>
+#define SCREEN_WIDTH 900
+#define SCREEN_HEIGHT 600
+
+using namespace std;
+
+SDL_Window* gWindow = NULL; // cửa sổ game
+SDL_Renderer* gRenderer = NULL; // màn hình xử lý
+Uint32 gOldTime; // tho
+float deltaTime = 0; // chênh lệch 2 frame
+
+
+// struct tọa độ 1 điểm
+class Vector2D
+{
+public:
+	float x;
+	float y;
+
+	Vector2D()
+	{
+		x = 0.0f;
+		y = 0.0f;
+	}
+
+	Vector2D(float a, float b)
+	{
+		x = a;
+		y = b;
+	}
+	Vector2D operator+(Vector2D other) {
+		return Vector2D(x + other.x, y + other.y);
+	}
+	Vector2D operator-(Vector2D other) {
+		return Vector2D(x - other.x, y - other.y);
+	}
+	Vector2D operator*(float t) {
+		return Vector2D(x*t, y*t);
+	}
+	Vector2D& operator+=(const Vector2D& other) {
+		x += other.x;
+		y += other.y;
+		return *this;
+	}
+};
+
+// khởi tạo sdl
+bool InitSDL()
+{
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+	Mix_Init(MIX_INIT_MP3);
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL did not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+	else
+	{
+		gWindow = SDL_CreateWindow("Games Engine Creation",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			cout << "Window was not created. Error : " << SDL_GetError();
+			return false;
+		}
+	}
+
+	gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+	if (gRenderer != NULL)
+	{
+		int imageFlags = IMG_INIT_PNG;
+		if (!(IMG_Init(imageFlags) & imageFlags))
+		{
+			cout << "SDL_Image could not initialise. Error: " << IMG_GetError();
+			return false;
+		}
+
+	}
+
+	else
+	{
+		cout << "Renderer could not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+
+	return true;
+
+
+}
+// đóng sdl
+void CloseSDL()
+{
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	IMG_Quit();
+	SDL_Quit();
+
+	SDL_DestroyRenderer(gRenderer);
+	gRenderer = NULL;
+	Mix_CloseAudio();
+	Mix_Quit();
+}
+// load ảnh
+SDL_Texture* LoadTextureFromFile(string path)
+{
+	SDL_Texture* pTexture = NULL;
+
+	SDL_Surface* pSurface = IMG_Load(path.c_str());
+	if (pSurface != NULL)
+	{
+		pTexture = SDL_CreateTextureFromSurface(gRenderer, pSurface);
+		if (pTexture == NULL)
+		{
+			cout << "Unable to create texture from surface. Error: " << SDL_GetError() << endl;
+		}
+		SDL_FreeSurface(pSurface);
+	}
+	else
+	{
+		cout << "Unable to create texture from surface. Error: " << IMG_GetError() << endl;
+	}
+	return pTexture;
+}
+// class lưu trữ khi load ảnh lên
+class Texture2D
+{
+public:
+	// size
+	float mWidth;
+	float mHeight;
+
+	SDL_Renderer* mRenderer; // màn hình hienr thị
+	Vector2D mPosition; // vị trí
+	SDL_Texture* mTexture; // biến lưu thông tin
+	Texture2D() {
+		mRenderer = NULL;
+		mTexture = NULL;
+		mWidth = 0;
+		mHeight = 0;
+	}
+	Texture2D(SDL_Renderer* renderer) { // khởi tạo
+		mRenderer = renderer;
+		mTexture = NULL;
+		mWidth = 0;
+		mHeight = 0;
+	}
+	void Free() { // xóa bộ nhớ
+		if (mTexture != NULL)
+		{
+			SDL_DestroyTexture(mTexture);
+			mTexture = NULL;
+		}
+		mWidth = 0;
+		mHeight = 0;
+	}
+	~Texture2D() { // giải phóng bộ nhớ
+		Free();
+
+		mRenderer = NULL;
+	}
+	bool LoadFromFile(string path) { // load ảnh
+		Free(); // giải phóng cũ
+		SDL_Surface* pSurface = IMG_Load(path.c_str());
+
+		if (pSurface != NULL)
+		{
+			SDL_SetColorKey(pSurface, SDL_TRUE, SDL_MapRGB(pSurface->format, 0, 0xFF, 0xFF)); // xóa nền
+
+			mWidth = pSurface->w; // giảm kích thước ban đầu
+			mHeight = pSurface->h;
+
+			mTexture = SDL_CreateTextureFromSurface(mRenderer, pSurface); 
+			if (mTexture == NULL)
+			{
+				cout << "Unable to create texture from surface. Error: " << SDL_GetError() << endl;
+			}
+			SDL_FreeSurface(pSurface);
+		}
+		else
+		{
+			cout << "Unable to create texture from surface. Error: " << IMG_GetError() << endl;
+		}
+		return mTexture != NULL;
+	}
+	// hiển thị đối tượng: vị trí, cách lấy đối xứng, góc xoay, phạm vi lấy, khung hiển thị
+	void Render(Vector2D p = { -1, -1 }, SDL_RendererFlip flip = SDL_FLIP_NONE, double angle = 0.0f, SDL_Rect* srcRect = nullptr, SDL_Rect* renderLocation = nullptr) { // load ảnh lên màn hình xử lý
+		SDL_SetRenderDrawColor(mRenderer, 0x00, 0x00, 0x00, 0x00);
+		// vẽ đối tượng lên màn hình xử lý, với khung vừa có, góc xoay angle, và cách lấy đối xứng flip
+		if (p.x == -1)
+			p = mPosition;
+		if (renderLocation == nullptr){
+			SDL_Rect r = { p.x, p.y, mWidth, mHeight };
+			SDL_RenderCopyEx(mRenderer, mTexture, srcRect, &r, angle, NULL, flip);
+		}
+		else {
+			SDL_RenderCopyEx(mRenderer, mTexture, srcRect, renderLocation, angle, NULL, flip);
+		}
+	}
+};
+static class Math {
+public:
+	static float Clamp(float value, float min, float max) {
+		if (value < min)
+			return min;
+		if (value > max)
+			return max;
+		return value;
+	}
+};
+
+int main(int argc, char* args[])
+{
+	if (InitSDL())
+	{
+		srand(time(0)); // cập nhật thời gian hiện tại để làm mới random
+		Mix_Chunk* sound = Mix_LoadWAV("./Sounds/BackGround.wav"); /// phát nhạc
+		Mix_PlayChannel(-1, sound, 0);
+		cout << "Setup";
+	}
+	CloseSDL();
+	return 0;
+}
